dad787c57132aaf92f1ea140b4b8bbc4634f4afa
pliskin <arieljanickcarvalho@gmail.com>
2022-10-29 13:52:53 +0100

    Input validation and functions.
    Added input validation with joi and cleaned up the code a bit by moving repetitive code over
    to a function.

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-const { response } = require("express");
-const Express = require("express");   
-                                      
-const app = Express()                 
+const Express = require("express"); 
+const Joi = require("joi");
+
+const app = Express();
 
 // Allow the parsin of json, otherwise you'll get undefined whenever you try to read
 // the body of a post request
 app.use(Express.json());
 
-// TODO: move find over to a function since it's being used more than once
 
 app.listen(3000);                     
 
@@ -27,7 +26,20 @@ const genres = [
         id: 3,
         genre: "drama"
     }
-]                  
+];
+
+function findGenre(id) {
+    return genres.find((genre) => genre.id === parseInt(id));
+}
+
+function validate(input) {
+    const schema = {
+        genre: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(input, schema);
+    return result;
+}
                                       
 app.get("/", (request, response) => { 
     response.send("Welcome to vidly");
@@ -39,7 +51,7 @@ app.get("/api/v1/genres", (request, response) => {
 
 app.get("/api/v1/genres/:id", (request, response) => {
     const id = request.params.id;
-    const genre = genres.find((genre) => genre.id === parseInt(id));
+    const genre = findGenre(id);
 
     if(!genre) {
         return response.status(404).send("No such genre");
@@ -52,6 +64,15 @@ app.get("/api/v1/genres/:id", (request, response) => {
 app.post("/api/v1/genres", (request, response) => {
     // Todo: Validate input later
     const genre = request.body.genre;
+    const input = request.body;
+
+    // You need to give it the input so it can validate it. Not the specific value you want to validate.
+    // Also, it's going to return either an error or the value. Never both
+    const result = validate(input);
+
+    if(result.error) {
+        return response.status(400).send(result.error.details[0].message);
+    }
 
     const createdGenre = {
         id: genres.length + 1,
@@ -64,13 +85,18 @@ app.post("/api/v1/genres", (request, response) => {
 });
 
 app.put("/api/v1/genres/:id", (request, response) => {
-    // Todo: Validate input later
     const id = request.params.id;
     const newGenre = request.body.genre;
-    const genre = genres.find((genre) => genre.id === parseInt(id));
+    const genre = findGenre(id);
+    const input = request.body;
 
     // if not found will be undefined
     if(!genre) return response.status(404).send("No such genre");
+
+    const result = validate(input);
+
+    // Check for error
+    if(result.error) return response.status(400).send(result.error.details[0].message);
 
     genre.genre = newGenre;
 
@@ -79,7 +105,7 @@ app.put("/api/v1/genres/:id", (request, response) => {
 
 app.delete("/api/v1/genres/:id", (request, response) => {
     const id = request.params.id;
-    const genre = genres.find((genre) => genre.id === parseInt(id));
+    const genre = findGenre(id);
     
     // If it doesn't exist no need to delete. Just return not found
     if(!genre) return response.status(404).send("No such genre");
